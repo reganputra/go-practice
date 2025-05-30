772d9e49c0b847cb37ad308325d8c1a5f4ec1667
reganputra <ramadhanregan334@gmail.com>
2025-05-31 05:30:18 +0700

    comment to help

--- go-pointers/pointers.go
+++ go-pointers/pointers.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	age := 30
 
-	agePointer := &age // Create a pointer to age
-	fmt.Println("Pointer to age:", *agePointer)
+	agePointer := &age                          // Create a pointer to age
+	fmt.Println("Pointer to age:", *agePointer) // Dereference the pointer to get the value
 
 	//fmt.Println(age)
 	adultAge(agePointer)
