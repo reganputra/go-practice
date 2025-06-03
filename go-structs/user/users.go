01b9cf4b266e8794a222299fb7a432044dcb172e
reganputra <ramadhanregan334@gmail.com>
2025-06-03 08:56:38 +0700

    refactor: exposing methods

--- go-structs/user/users.go
+++ go-structs/user/users.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	FirstName string
-	LastName  string
-	Birthdate string
+	firstName string
+	lastName  string
+	birthdate string
 	createdAt time.Time
 }
 
@@ -22,22 +22,22 @@ func NewUser(firstName, lastName, birthdate string) (*User, error) {
 	}
 
 	return &User{
-		FirstName: firstName,
-		LastName:  lastName,
-		Birthdate: birthdate,
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
 		createdAt: time.Now(),
 	}, nil
 }
 
 func (user *User) OutputDetail() {
 	fmt.Println("Outputting user details...")
-	fmt.Println("First Name:", user.FirstName)
-	fmt.Println("Last Name:", user.LastName)
-	fmt.Println("Birthdate:", user.Birthdate)
+	fmt.Println("First Name:", user.firstName)
+	fmt.Println("Last Name:", user.lastName)
+	fmt.Println("Birthdate:", user.birthdate)
 }
 
 func (user *User) Remove() {
 	fmt.Println("Removing user...")
-	user.FirstName = ""
-	user.LastName = ""
+	user.firstName = ""
+	user.lastName = ""
 }
