f901311586161f0737c7abcb241965e5ea22562d
reganputra <ramadhanregan334@gmail.com>
2025-06-06 05:03:48 +0700

    slicing array

--- go-array-slices-map/array.go
+++ go-array-slices-map/array.go
@@ -4,15 +4,16 @@ import "fmt"
 
 func main() {
 
-	var productNames [4]string = [4]string{"A Book"}
+	var productNames = [4]string{"A Book"}
 	fmt.Println(productNames)
-	arr := [5]int{1, 2, 3, 4, 5.}
+	arr := [5]float64{1.23, 2.12, 3.14, 4.20, 55.}
 	fmt.Println(arr[0])
-	//arr[2] = 13
 
 	// Display the array elements
 	//for i: = 0; i < len(productNames); i++ {
 	//	println(productNames[i])
 	//}
 
+	featuredArr := arr[1:3]
+	fmt.Println(featuredArr)
 }
