a99a3a0883f839fe867683ef644960a6dd70e52d
reganputra <ramadhanregan334@gmail.com>
2025-06-07 05:28:49 +0700

    declaring maps

---
+++ go-array-slices-map/map.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	webUrl := map[string]string{
+		"google":   "https://www.google.com",
+		"facebook": "https://www.facebook.com",
+	}
+	fmt.Println(webUrl)
+}
