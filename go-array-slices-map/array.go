0dce537ea1bbad5f1a819bde69c1f211202d37aa
reganputra <ramadhanregan334@gmail.com>
2025-06-06 07:42:16 +0700

    comment for slice

--- go-array-slices-map/array.go
+++ go-array-slices-map/array.go
@@ -6,14 +6,15 @@ func main() {
 
 	var productNames = [4]string{"A Book"}
 	fmt.Println(productNames)
-	arr := [5]float64{1.23, 2.12, 3.14, 4.20, 55.}
-	fmt.Println(arr[0])
+	prices := [5]float64{1.23, 2.12, 3.14, 4.20, 5.15}
+	fmt.Println(prices[0])
 
 	// Display the array elements
 	//for i: = 0; i < len(productNames); i++ {
 	//	println(productNames[i])
 	//}
 
-	featuredArr := arr[1:3]
-	fmt.Println(featuredArr)
+	featurePrices := prices[1:]
+	morePrices := featurePrices[:4]
+	fmt.Println(len(morePrices), cap(featurePrices)) // Slices take the original memory of the array
 }
