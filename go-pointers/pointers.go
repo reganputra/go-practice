4b41ad05e8f3a6d72ab2c64d72cf9a8f2357047b
reganputra <ramadhanregan334@gmail.com>
2025-06-08 05:32:48 +0700

    additional pointers example

--- go-pointers/pointers.go
+++ go-pointers/pointers.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type City struct {
+	Name       string
+	Population int
+}
+
 func main() {
 	age := 30
 
@@ -11,8 +16,24 @@ func main() {
 	//fmt.Println(age)
 	adultAge(agePointer)
 	fmt.Println(age)
+
+	city := City{"New York", 8000000}
+	city2 := &city
+	city2.Name = "London"
+	changePopulation(city2, 10000) // Change the population using a pointer to the City struct
+	fmt.Println(city)
+	city.PrintPopulation()
 }
 
 func adultAge(age *int) {
 	*age = *age - 18 // Dereference the pointer to modify the value
+}
+
+func changePopulation(city *City, population int) {
+	// Dereference the pointer to modify the value
+	city.Population = population
+}
+
+func (populations *City) PrintPopulation() {
+	fmt.Println("Population of", populations.Name, "is", populations.Population)
 }
