87cdc2f52046e60ca2fef7a408290626044fb560
reganputra <ramadhanregan334@gmail.com>
2025-06-07 19:24:44 +0700

    working multiple channel

--- goroutine/main.go
+++ goroutine/main.go
@@ -5,21 +5,26 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true // signal that the task is done
+	close(doneChan)  // close the channel to indicate no more messages will be sent
 }
 
 func main() {
-	go greet("Nice to meet you!")
-	go greet("How are you?")
 	done := make(chan bool)
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
-	go greet("I hope you're liking the course!")
-	<-done // wait for the slowGreet to finish
+	go greet("I hope you're liking the course!", done)
+
+	for range done {
+		fmt.Println("Task is done!")
+	}
 }
