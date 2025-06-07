7c1a8ea63b1c03994e828ba29323421fa795c464
reganputra <ramadhanregan334@gmail.com>
2025-06-07 09:05:40 +0700

    return function as values

--- go-advance-function/functionAsValues.go
+++ go-advance-function/functionAsValues.go
@@ -6,10 +6,20 @@ type transformFunc func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
+	moreNumbers := []int{6, 7, 8, 9, 10}
 	dbNumbers := transformNumber(&numbers, doubleNumber)
 	trNumbers := transformNumber(&numbers, tripleNumber)
 	fmt.Println(dbNumbers)
 	fmt.Println(trNumbers)
+
+	transform1 := getTransformNumber(&numbers)
+	trNumbers2 := getTransformNumber(&moreNumbers)
+
+	transform3 := transformNumber(&numbers, transform1)
+	transform4 := transformNumber(&moreNumbers, trNumbers2)
+	fmt.Println(transform3)
+	fmt.Println(transform4)
+
 }
 
 func transformNumber(num *[]int, transform transformFunc) []int {
@@ -18,6 +28,14 @@ func transformNumber(num *[]int, transform transformFunc) []int {
 		dbNumbers = append(dbNumbers, transform(value))
 	}
 	return dbNumbers
+}
+
+func getTransformNumber(num *[]int) transformFunc {
+	if (*num)[0] == 1 {
+		return doubleNumber
+	} else {
+		return tripleNumber
+	}
 }
 
 func doubleNumber(num int) int {
