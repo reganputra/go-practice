9aa28e344fc9b80a2a1ba9865107f7bb62d72fa4
reganputra <ramadhanregan334@gmail.com>
2025-06-07 13:47:09 +0700

    adding method

--- price-calculator/main.go
+++ price-calculator/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"price-calculator/prices"
+)
+
 func main() {
 
+	taxRates := []float64{0.05, 0.1, 0.15}
+
+	for _, rate := range taxRates {
+		job := prices.NewTaxIncludedPriceJob(rate)
+		job.Process()
+	}
 }
--- price-calculator/prices/prices.go
+++ price-calculator/prices/prices.go
@@ -1,5 +1,7 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
@@ -9,6 +11,16 @@ type TaxIncludedPriceJob struct {
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
 		InputPrices: []float64{10, 20, 30},
-		TaxRate:     taxRate, // Default tax rate of 20%
+		TaxRate:     taxRate,
 	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+
+	fmt.Println(result)
 }
