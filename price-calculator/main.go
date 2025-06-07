e062a648d5b75533dd718483e0cb52bba8b3ccc1
reganputra <ramadhanregan334@gmail.com>
2025-06-07 19:48:10 +0700

    channel error handling

--- price-calculator/main.go
+++ price-calculator/main.go
@@ -9,12 +9,18 @@ import (
 func main() {
 	taxRates := []float64{0.05, 0.1, 0.15}
 	doneChan := make([]chan bool, len(taxRates))
+	errorChan := make([]chan error, len(taxRates))
 	for i, taxRate := range taxRates {
 		doneChan[i] = make(chan bool)
+		errorChan[i] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		//cmd := cmdmanager.New()
 		job := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		go job.Process(doneChan[i])
+	}
+
+	for _, errChan := range errorChan {
+		errChan <- nil // In a real application, you would handle errors here
 	}
 	for _, done := range doneChan {
 		<-done
