6559feb4e6e06caac83a8880de76a09f64be9238
reganputra <ramadhanregan334@gmail.com>
2025-06-07 07:30:47 +0700

    loop for arrays, slices, maps

--- go-array-slices-map/main.go
+++ go-array-slices-map/main.go
@@ -2,24 +2,39 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64 // Type alias
+
+func (f *floatMap) output() {
+	fmt.Println(*f)
+}
+
 func main() {
+
+	// Slices
 	names := make([]string, 3, 5)
 
-	//names = append(names, "John", "Jane", "Jessica")
 	names[0] = "John"
 	names[1] = "Jane"
 	names[2] = "Jessica"
 	names = append(names, "Jack", "Jill")
-	fmt.Println(names)
-	fmt.Println(len(names))
+	//fmt.Println(names)
+	//fmt.Println(len(names))
+	for index, value := range names {
+		fmt.Printf("Index: %d, Value: %s\n", index, value)
+	}
 
-	courses := make(map[string]string, 5)
+	// Maps
+	courses := make(floatMap, 5)
 
-	courses["Go"] = "Go Programming Language"
-	courses["Python"] = "Python Programming Language"
-	courses["Java"] = "Java Programming Language"
-	courses["JavaScript"] = "JavaScript Programming Language"
-	courses["C++"] = "C++ Programming Language"
-	fmt.Println(len(courses))
+	courses["Go"] = 5.33
+	courses["Python"] = 15.55
+	courses["Java"] = 25.99
+	courses["JavaScript"] = 35.75
+	courses["C++"] = 45.00
+	//courses.output()
+
+	for key, value := range courses {
+		fmt.Printf("Course: %s, Price: %.2f\n", key, value)
+	}
 
 }
